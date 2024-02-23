4a7216432ac28de807427a4599cf7dd544ba0e50
Anargha John <anargha.john@prompttechsolutions.com>
2024-02-23 18:25:59 +0530

    fix

--- src/screens/Dashboard/BottomTabs.tsx
+++ src/screens/Dashboard/BottomTabs.tsx
@@ -1,3 +1,114 @@
+// import React, {useEffect, useState} from 'react';
+// import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+// import {Image, Text, View} from 'react-native-ui-lib';
+// import {RouteProp} from '@react-navigation/native';
+// import {NativeStackNavigationProp} from '@react-navigation/native-stack';
+// import {ImageSourcePropType, StyleSheet} from 'react-native';
+// import {RootStackParams, RouteNames} from '../../navigation';
+// import AppImages from '../../constants/AppImages';
+// import AppColors from '../../constants/AppColors';
+// import AppFonts from '../../constants/AppFonts';
+// import HomeScreen from '../home/HomeScreen';
+// import MyTripScreen from '../mytrip/MyTripScreen';
+// import AddTripScreen from '../addtrip/AddTripScreen';
+// import ProfileScreen from '../profile/ProfileScreen';
+
+// const Tab = createBottomTabNavigator();
+
+// export type BottomTabsNavigationProps = NativeStackNavigationProp<
+//   RootStackParams,
+//   'BottomTabs'
+// >;
+
+// export type BottomTabsRouteProps = RouteProp<RootStackParams, 'BottomTabs'>;
+
+// interface Props {}
+
+// const BottomTabs: React.FC<Props> = () => {
+//   const renderView = (focused: boolean, title: string, source: any) => {
+//     return (
+//       <View center>
+//         <Image
+//           source={source}
+//           resizeMode="contain"
+//           style={{
+//             width: 24,
+//             height: 24,
+//             tintColor: focused ? AppColors.Orange : 'white',
+//           }}
+//         />
+//         <Text
+//           style={[styles.text, {color: focused ? AppColors.Orange : 'white'}]}>
+//           {title}
+//         </Text>
+//       </View>
+//     );
+//   };
+
+//   return (
+//     <Tab.Navigator
+//       screenOptions={{
+//         headerShown: false,
+//         tabBarShowLabel: false,
+//         tabBarStyle: {
+//           position: 'absolute',
+//           height: 80,
+//           backgroundColor: AppColors.Black,
+//           borderTopLeftRadius: 34,
+//           borderTopRightRadius: 34,
+//           borderWidth:1,
+//           borderColor:AppColors.Black,
+//           elevation:10
+//         },
+//       }}>
+//       <Tab.Screen
+//         name={RouteNames.HomeScreen}
+//         component={HomeScreen}
+//         options={{
+//           tabBarIcon: ({focused}) =>
+//             renderView(focused, 'Home', AppImages.HOME),
+//         }}
+//       />
+//       <Tab.Screen
+//         name={RouteNames.MyTripScreen}
+//         component={MyTripScreen}
+//         options={{
+//           tabBarIcon: ({focused}) =>
+//             renderView(focused, 'My Trips', AppImages.JEEP),
+//         }}
+//       />
+
+//       <Tab.Screen
+//         name={RouteNames.AddTripScreen}
+//         component={AddTripScreen}
+//         options={{
+//           tabBarIcon: ({focused}) =>
+//             renderView(focused, 'Add Trip', AppImages.ADDTRIP),
+//         }}
+//       />
+
+//       <Tab.Screen
+//         name={RouteNames.ProfileScreen}
+//         component={ProfileScreen}
+//         options={{
+//           tabBarIcon: ({focused}) =>
+//             renderView(focused, 'Profile', AppImages.PROFILE),
+//         }}
+//       />
+//     </Tab.Navigator>
+//   );
+// };
+
+// export default BottomTabs;
+
+// const styles = StyleSheet.create({
+//   text: {
+//     fontFamily: AppFonts.REGULAR,
+//     fontSize: 14,
+//     textAlign: 'center',
+//   },
+// });
+
 import React, { useState } from 'react';
 import HomeScreen from '../home/HomeScreen';
 import MyTripScreen from '../mytrip/MyTripScreen';
